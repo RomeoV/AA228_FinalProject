650ad346076f491b8a30736070d683dd14157231
Romeo Valentin <romeov@stanford.edu>
2023-03-02 21:31:52 -0800

    Separate value iteration and policy evaluation and implement greedy policy from Value function
    Co-authored-by: Hugo Buurmeijer <hbuurmeijer@users.noreply.github.com>

--- src/eval.jl
+++ src/eval.jl
@@ -17,7 +17,7 @@ function eval_problem(nx::Int, agent_strategy_p::Float64, transition_model::Any;
     return policy_value
 end
 
-function value_iteration(mdp::DroneSurveillanceMDP, policy::Policy;
+function policy_evaluation(mdp::DroneSurveillanceMDP, policy::Policy;
                          trace_state::Union{Nothing, DSState}=nothing)
     nx, ny = mdp.size
     γ = mdp.discount_factor
@@ -45,4 +45,52 @@ function value_iteration(mdp::DroneSurveillanceMDP, policy::Policy;
         !isnothing(trace_state) && @info U[trace_state]
     end
     return U
+end
+
+
+function value_iteration(mdp::DroneSurveillanceMDP;
+                         trace_state::Union{Nothing, DSState}=nothing)
+    nx, ny = mdp.size
+    γ = mdp.discount_factor
+    nonterminal_states = [DSState([qx, qy], [ax, ay])
+                          for qx in 1:nx,
+                              qy in 1:ny,
+                              ax in 1:nx,
+                              ay in 1:ny][:]  # note that we flatten the array in the end
+
+    U = Dict(s=>rand() for s in nonterminal_states)
+    U[mdp.terminal_state] = reward(mdp, mdp.terminal_state, rand(ACTION_DIRS))
+    for i in 1:1000
+        # I benchmarked these (cache misses?) but they're about the same.
+        # So we use the Gauss-Seidl version, which should converge faster.
+        # U_ = U  # Alternative:
+        U_ = copy(U)
+        @floop for s in nonterminal_states
+            U[s] = maximum(
+                     a -> let r = reward(mdp, s, a),
+                              # note that we use the true transition model here!
+                              T_probs = DroneSurveillance.transition(mdp, mdp.agent_strategy, DSPerfectModel(), s, a),
+                              T_probs_iter = weighted_iterator(T_probs)
+                         r + γ * sum(p*U_[s_] for (s_, p) in T_probs_iter)
+                     end,
+                     ACTION_DIRS)
+        end
+        !isnothing(trace_state) && @info U[trace_state]
+    end
+    return U
+end
+
+function runtime_policy(mdp, U, s)
+    γ = mdp.discount_factor
+    U_a = Dict(
+        a=>let r = reward(mdp, s, a),
+            # note that we use the true transition model here!
+            T_probs = DroneSurveillance.transition(mdp, mdp.agent_strategy, DSPerfectModel(), s, a),
+            T_probs_iter = weighted_iterator(T_probs)
+            r + γ * sum(p*U[s_] for (s_, p) in T_probs_iter)
+        end
+        for a in ACTION_DIRS
+    )
+    val, a = findmax(U_a)
+    return a
 end
