e344c3123ac0422fedeb75d92f25c3c6de0dc93a
Romeo Valentin <romeov@stanford.edu>
2023-03-07 14:46:31 -0800

    Create visualization with approx model option

--- scripts/make_visualization.jl
+++ scripts/make_visualization.jl
@@ -1,7 +1,20 @@
 using Revise, AA228_FinalProject, DroneSurveillance, POMDPs, POMDPTools,
       Distributions, POMDPGifs, Cairo
-mdp = DroneSurveillanceMDP{PerfectCam}(size=(10, 10), agent_strategy=DSAgentStrat(3//4));
 AA = AA228_FinalProject;
-U = AA.value_iteration(mdp);
+plan_using_approx_model = true
+
+mdp = DroneSurveillanceMDP{PerfectCam}(size=(10, 10),
+                                       agent_strategy=DSAgentStrat(1//4));
+T_model = if plan_using_approx_model
+    create_linear_transition_model(mdp)
+else
+    DSPerfectModel()
+end
+
+U = let mdp=deepcopy(mdp)
+    mdp.transition_model = T_model
+    U = AA.value_iteration(mdp);
+end
+@assert mdp.transition_model isa DSPerfectModel
 pol = POMDPTools.FunctionPolicy(s->AA.runtime_policy(mdp, U, s));
 makegif(mdp, pol, filename="/tmp/out.gif")
